7660aeeebc2be11f57c678cea1b760535bfc278c
hdGuild <contact.laguilde@helldorado.fr>
2020-05-05 13:55:41 +0200

    adding ssh-key search on Digital Ocean

--- doctl.ps1
+++ doctl.ps1
@@ -3,6 +3,13 @@
 # authentificate with PAT
 doctl auth init
 
+# listing certificates available for account 
+doctl compute ssh-key list -o "json"
+#getting the list in an object to be parsed
+$myObjDOSSHkeys = doctl compute ssh-key list -o "json" | ConvertFrom-Json
+# parsing the object on "Eurynome" name
+$myObjDOSSHkeys | Where-Object -property name -match "Eurynome"
+
 # listing public images 
 doctl compute image list --public -o "json"
 # getting the list in an obkect to be parsed 
