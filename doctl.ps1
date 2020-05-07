a900ee9869e72dda41cd87100167b0bede4b3209
hdGuild <contact.laguilde@helldorado.fr>
2020-05-07 18:54:41 +0200

    adding api search for image size

--- doctl.ps1
+++ doctl.ps1
@@ -12,7 +12,18 @@ $myObjDOSSHkeys | Where-Object -property name -match "Eurynome"
 
 # listing public images 
 doctl compute image list --public -o "json"
-# getting the list in an obkect to be parsed 
+# getting the list in an object to be parsed 
 $MyObjDOImageList = doctl compute image list --public -o "json"  | ConvertFrom-Json
 # parsing the object on "CentOS" distributions
 $MyObjDOImageList | Where-Object -property Distribution -Match "CentOS"
+
+# listing available size for server
+doctl compute size list -o "json"
+# getting the list in an object to be parsed 
+$myObjDOimageSize = doctl compute size list -o "json" | ConvertFrom-Json
+# parsing the object on images sizes available on FRA1 region, sorted by cost
+$myObjDOimageSize | Where-Object -property regions -Match "FRA1" `
+                  | where-Object -Property price_hourly -LE 0.1 `
+                  | Where-Object -property Memory -GE 1024 `
+                  | Sort-Object -Property price_monthly -Descending
+
