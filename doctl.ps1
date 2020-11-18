a062f3c650bbf450b514e8b5eadf1a0b74a11f34
hdGuild <contact.laguilde@helldorado.fr>
2020-11-18 16:19:58 +0100

    parsing object with filters

--- doctl.ps1
+++ doctl.ps1
@@ -16,6 +16,10 @@ doctl compute image list --public -o "json"
 $MyObjDOImageList = doctl compute image list --public -o "json"  | ConvertFrom-Json
 # parsing the object on "CentOS" distributions
 $MyObjDOImageList | Where-Object -property Distribution -Match "CentOS"
+# parsing the object on "Ubuntu" distributions and "LEMP" description
+$MyObjDOImageList | Where-Object -property Distribution -Match "Ubuntu" | Where-Object -property Description -match "LEMP"
+$MyObjDOImageList | Where-Object -property slug -match "ubuntu-18-04-x64"
+
 
 # listing available size for server
 doctl compute size list -o "json"
@@ -26,4 +30,7 @@ $myObjDOimageSize | Where-Object -property regions -Match "FRA1" `
                   | where-Object -Property price_hourly -LE 0.1 `
                   | Where-Object -property Memory -GE 1024 `
                   | Sort-Object -Property price_monthly -Descending
+                  # the cheaper is $5 monthly
 
+# search cost from image size
+$myObjDOimageSize | Where-Object -Property slug -EQ "2Gb" # $20 monthly
